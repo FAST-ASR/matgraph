6c4539c81fe0a9ac492da4050e8afc327183f300
Lucas Ondel <lucas.ondel@gmail.com>
2022-06-10 08:38:06 +0000

    added LM functions

--- matgraph/__init__.py
+++ matgraph/__init__.py
@@ -3,10 +3,12 @@ import torch
 from juliacall import Main as jl
 
 # Set the Julia environment.
+jl.seval("using Serialization")
 jl.seval("using CUDA")
 jl.seval("using DLPack")
 jl.seval("using MarkovModels")
 jl.seval("using Semirings")
+jl.seval("using SparseArrays")
 jl.seval("using PythonCall")
 jl.seval("""
 transfer(x, fn::PythonCall.Py) =
@@ -16,6 +18,5 @@ transfer(x, fn::PythonCall.Py) =
 def transfer(x):
     return jl.transfer(x, torch.to_dlpack)
 
-from .fsm import fsm_from_json, union, cat, compose, determinize, minimize, \
-                 propagate, renorm
+from .fsm import *
 
--- matgraph/fsm.py
+++ matgraph/fsm.py
@@ -1,10 +1,14 @@
 # SPDX-License-Identifier: MIT
 
-import json
+import math
+from collections.abc import MutableMapping
 from juliacall import Main as jl
 
 def fsm_from_json(jsonstr):
     return jl.FSM(jsonstr)
+
+def Label(x):
+    return jl.Label(x)
 
 def union(fsm1, fsm2):
     return jl.union(fsm1, fsm2)
@@ -12,16 +16,8 @@ def union(fsm1, fsm2):
 def cat(fsm1, fsm2):
     return jl.cat(fsm1, fsm2)
 
-jl.seval("""
-MarkovModels.compose(fsm1, fsms, sep) =
-    compose(fsm1, pyconvert(Array, fsms), sep)
-""")
-def compose(fsm1, fsms, sep=""):
-    if not sep:
-        sep = jl.one(jl.UnionConcatSemiring)
-    else:
-        sep = jl.Label(sep)
-    return jl.compose(fsm1, fsms, sep)
+def compose(fsm1, fsms):
+    return jl.compose(fsm1, fsms)
 
 def determinize(fsm):
     return jl.determinize(fsm)
@@ -34,3 +30,66 @@ def propagate(fsm):
 
 def renorm(fsm):
     return jl.renorm(fsm)
+
+def fsmdict():
+    return jl.Dict()
+
+def linear_fsm(seq, precision="Float32", init_sil_prob=0, sil_prob=0,
+               final_sil_prob=0, sil="sil"):
+    """Create a linear FSM from `seq`."""
+    if init_sil_prob > 0:
+        init = [[1, math.log(init_sil_prob)], [2, math.log(1 - init_sil_prob)]]
+        labels = [sil, seq[0]]
+        arcs = [[1, 2, 0]]
+        nstates = 2
+    else:
+        init = [[1, 0]]
+        labels = [seq[0]]
+        nstates = 1
+        arcs = []
+
+    for i in range(1, len(seq)):
+        if sil_prob > 0:
+            arcs.append([nstates, nstates+1, math.log(sil_prob)])
+            arcs.append([nstates, nstates+2, math.log(1 - sil_prob)])
+            arcs.append([nstates+1, nstates+2, 0])
+            labels.append(sil)
+            labels.append(seq[i])
+            nstates += 2
+        else:
+            arcs.append([nstates, nstates+1, 0])
+            labels.append(seq[i])
+            nstates += 1
+
+    if final_sil_prob > 0:
+        final = [[nstates, math.log(1-final_sil_prob)], [nstates+1, 0]]
+        arcs.append([nstates, nstates+1, math.log(final_sil_prob)])
+        labels.append(sil)
+    else:
+        final = [[nstates, 0]]
+
+
+    topo = f"""
+    {{
+        "semiring": "LogSemiring{{{precision}}}",
+        "initstates": {init},
+        "arcs": {arcs},
+        "finalstates": {final},
+        "labels": ["{'","'.join(labels)}"]
+    }}
+    """
+    return renorm(fsm_from_json(topo))
+
+def save(path, obj):
+    jl.serialize(path, obj)
+
+def load(path):
+    return jl.deserialize(path)
+
+jl.seval("mergengrams(ng1, ng2) = mergewith((x, y) -> x .+ y, ng1, ng2) ")
+def merge_ngrams(ng1, ng2):
+    return jl.mergengrams(ng1, ng2)
+
+def ngrams(fsm, order=2):
+    return jl.totalngramsum(fsm, order = order)
+
